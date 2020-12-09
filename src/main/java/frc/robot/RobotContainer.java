2989b194ebf0993f593ae5c67cc7eb1365f325af
Brian Utterback <brian@utterback.org>
2020-12-09 17:16:39 -0500

    Add comments.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,7 @@ import frc.robot.commands.DriveCommand;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
-
+    /** Same old tune. Put the robot together here. **/
     private final PWMVictorSPX m_leftmotor = new PWMVictorSPX(DriveConstants.kLeftMotorPort);
     private final PWMVictorSPX m_rightmotor = new PWMVictorSPX(DriveConstants.kRightMotorPort);
     private final DriveSubsystem m_drivesubsystem = new DriveSubsystem(m_leftmotor, m_rightmotor);
@@ -20,11 +20,12 @@ public class RobotContainer {
     private final Command m_drivecommand;
     
     public RobotContainer() {
- 
+        /** Instantiate the commands and the subsystems and store them for later. **/
         m_autocommand = new AutoCommand(m_drivesubsystem);
         m_drivecommand = new DriveCommand(m_drivesubsystem, m_stick);
         m_drivesubsystem.setDefaultCommand(m_drivecommand);
     }
+    /** We add a getter function so we can get the autonomous command in the Robot.java file **/
     public Command getAutonomousCommand() {
         return m_autocommand;
     }
