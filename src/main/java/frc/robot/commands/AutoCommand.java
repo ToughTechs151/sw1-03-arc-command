2f132f0648b249eaadf4ca82e3708f07266a94b3
Brian Utterback <brian@utterback.org>
2020-12-09 17:45:48 -0500

    add comments.

--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -9,6 +9,20 @@ import frc.robot.subsystems.DriveSubsystem;
 public class AutoCommand extends SequentialCommandGroup {
     private final DriveSubsystem m_subsystem;
 
+    /**
+     * In the autonomous command we want to delay a little while after autonmous
+     * mode begins, then set the drive to a certain speed for a set period of time,
+     * and then trun off the moter. We can see how we can use the Sequential command
+     * group to build the automomous command out of separate instantiations of the
+     * other DrivecCommand. And we use the withTimeout decorator to terminate the 
+     * subcommand after a given period of time. 
+     *
+     * There is more than one way to add command to a sequential command group, 
+     * but this method is the easiest and most clear. There are several methods
+     * in the WPIlib that can be used to put command together into more complex 
+     * commands and many of them have decorators like withTimeout to modify the
+     * way they are executed.
+     **/
     public AutoCommand(final DriveSubsystem subsystem) {
         addRequirements(subsystem);
         m_subsystem = subsystem;
